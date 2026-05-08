bhardwajRahul/coturn

commit 4156e6abf450773f107cbadc4d941aa5cc99f0f3

Fix Linux build warnings (#1853)

src/apps/relay/prom_server.c
@@ -78,11 +78,18 @@ prom_gauge_t *turn_total_allocations;
 #define MHD_RESULT int
 #endif
 
-MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method,
-                            const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls) {
+static MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method,
+                                   const char *version, const char *upload_data, size_t *upload_data_size,
+                                   void **con_cls) {
   MHD_RESULT ret;
+  (void)cls;
+  (void)version;
+  (void)upload_data;
+  (void)upload_data_size;
+  (void)con_cls;
 
   char *body = "not found";
+  char *owned_body = NULL;
   enum MHD_ResponseMemoryMode mode = MHD_RESPMEM_PERSISTENT;
   unsigned int status = MHD_HTTP_NOT_FOUND;
 
@@ -90,7 +97,15 @@ MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection, const 
     status = MHD_HTTP_METHOD_NOT_ALLOWED;
     body = "method not allowed";
   } else if (strcmp(url, turn_params.prometheus_path) == 0) {
-    body = prom_collector_registry_bridge(PROM_COLLECTOR_REGISTRY_DEFAULT);
+    const char *prom_body = prom_collector_registry_bridge(PROM_COLLECTOR_REGISTRY_DEFAULT);
+    if (prom_body == NULL) {
+      return MHD_NO;
+    }
+    owned_body = strdup(prom_body);
+    if (owned_body == NULL) {
+      return MHD_NO;
+    }
+    body = owned_body;
     mode = MHD_RESPMEM_MUST_FREE;
     status = MHD_HTTP_OK;
   } else if (strcmp(url, "/") == 0) {
@@ -103,7 +118,7 @@ MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection, const 
   struct MHD_Response *response = MHD_create_response_from_buffer(strlen(body), body, mode);
   if (response == NULL) {
     if (mode == MHD_RESPMEM_MUST_FREE) {
-      free(body);
+      free(owned_body);
     }
     ret = MHD_NO;
   } else {

src/apps/natdiscovery/natdiscovery.c
@@ -713,7 +713,7 @@ int main(int argc, char **argv) {
     exit(-1);
   }
 
-  if (local_port >= 0 && !strcmp(local_addr_string, "\0")) {
+  if (local_port != 0 && !strcmp(local_addr_string, "\0")) {
     fprintf(stderr, "To use local port please specify local address \"-L\"!\n");
     fprintf(stderr, "We need to know the address familly to set the port.\n");
     exit(-1);

src/apps/relay/dtls_listener.c
@@ -156,11 +156,11 @@ static size_t print_packet_txt2pcap(uint64_t now, uint8_t *payload, size_t paylo
   index += snprintf((char *)(txt2pcap + index), txt2pcap_length - index, " 0000");
 
   for (size_t i = 0; i < payload_length; i++) {
-    size_t n = snprintf((char *)(txt2pcap + index), txt2pcap_length - index, " %02x", payload[i]);
-    if (n < 0 || n >= txt2pcap_length - index) {
+    int n = snprintf((char *)(txt2pcap + index), txt2pcap_length - index, " %02x", payload[i]);
+    if (n < 0 || (size_t)n >= txt2pcap_length - index) {
       break;
     }
-    index += n;
+    index += (size_t)n;
   }
   index += snprintf((char *)(txt2pcap + index), txt2pcap_length - index, " # STUN_PACKET ");
   return index;

src/apps/relay/http_server.c
@@ -62,29 +62,32 @@ static void write_http_echo(ioa_socket_handle s) {
     if ((sat == HTTP_CLIENT_SOCKET) || (sat == HTTPS_CLIENT_SOCKET)) {
       char content_http[1025];
       const char *const title = "TURN Server";
-      snprintf(content_http, sizeof(content_http) - 1,
-               "<!DOCTYPE html>\r\n<html>\r\n  <head>\r\n    <title>%s</title>\r\n  </head>\r\n  <body>\r\n    "
-               "<b>%s</b> <br> <b><i>use https connection for the admin session</i></b>\r\n  </body>\r\n</html>\r\n",
-               title, title);
-
-      char data_http[1025];
-      snprintf(data_http, sizeof(data_http) - 1,
-               "HTTP/1.0 200 OK\r\nServer: %s\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: "
-               "%zu\r\n\r\n%.906s",
-               TURN_SOFTWARE, strlen(content_http), content_http);
+      const int content_len = snprintf(
+          content_http, sizeof(content_http),
+          "<!DOCTYPE html>\r\n<html>\r\n  <head>\r\n    <title>%s</title>\r\n  </head>\r\n  <body>\r\n    "
+          "<b>%s</b> <br> <b><i>use https connection for the admin session</i></b>\r\n  </body>\r\n</html>\r\n",
+          title, title);
+      if (content_len < 0) {
+        return;
+      }
 
       ioa_network_buffer_handle nbh_http = ioa_network_buffer_allocate(s->e);
       char *data = (char *)ioa_network_buffer_data(nbh_http);
       size_t cap = ioa_network_buffer_get_capacity(nbh_http);
-      size_t len = strlen(data_http) + 1;
-      if (len > cap) {
-        len = cap;
+      const int response_len =
+          snprintf(data, cap,
+                   "HTTP/1.0 200 OK\r\nServer: %s\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: "
+                   "%d\r\n\r\n%s",
+                   TURN_SOFTWARE, content_len, content_http);
+      if (response_len < 0) {
+        ioa_network_buffer_delete(s->e, nbh_http);
+        return;
       }
-      if (len > 0) {
-        memcpy(data, data_http, len);
-        data[len - 1] = '\0';
+      size_t len = (size_t)response_len;
+      if (len >= cap) {
+        len = cap - 1;
       }
-      ioa_network_buffer_set_size(nbh_http, len > 0 ? len - 1 : 0);
+      ioa_network_buffer_set_size(nbh_http, len);
       send_data_from_ioa_socket_nbh(s, NULL, nbh_http, TTL_IGNORE, TOS_IGNORE, NULL);
     }
   }

src/apps/relay/netengine.c
@@ -906,27 +906,6 @@ static void listener_receive_message(struct bufferevent *bev, void *ptr) {
   }
 }
 
-// <<== communications between listener and relays
-
-static ioa_engine_handle create_new_listener_engine(void) {
-  struct event_base *eb = turn_event_base_new();
-  super_memory_t *sm = new_super_memory_region();
-  ioa_engine_handle e =
-      create_ioa_engine(sm, eb, turn_params.listener.tp, turn_params.relay_ifname, turn_params.relays_number,
-                        turn_params.relay_addrs, turn_params.default_relays, turn_params.verbose
-#if !defined(TURN_NO_HIREDIS)
-                        ,
-                        &turn_params.redis_statsdb
-#endif
-      );
-  if (!e) {
-    exit(-1);
-  }
-  set_ssl_ctx(e, &turn_params);
-  ioa_engine_set_rtcp_map(e, turn_params.listener.rtcpmap);
-  return e;
-}
-
 static void setup_listener(void) {
   super_memory_t *sm = new_super_memory_region();
 

commit 238c311f05bce1049ef6b8ef86de77707e481115

Fix Prometheus metrics response leak (#1900)
Fix memory leak introduced in #1853

Resolved #1898

src/apps/relay/prom_server.c
@@ -89,7 +89,6 @@ static MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection,
   (void)con_cls;
 
   char *body = "not found";
-  char *owned_body = NULL;
   enum MHD_ResponseMemoryMode mode = MHD_RESPMEM_PERSISTENT;
   unsigned int status = MHD_HTTP_NOT_FOUND;
 
@@ -97,15 +96,10 @@ static MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection,
     status = MHD_HTTP_METHOD_NOT_ALLOWED;
     body = "method not allowed";
   } else if (strcmp(url, turn_params.prometheus_path) == 0) {
-    const char *prom_body = prom_collector_registry_bridge(PROM_COLLECTOR_REGISTRY_DEFAULT);
-    if (prom_body == NULL) {
+    body = (char *)prom_collector_registry_bridge(PROM_COLLECTOR_REGISTRY_DEFAULT);
+    if (body == NULL) {
       return MHD_NO;
     }
-    owned_body = strdup(prom_body);
-    if (owned_body == NULL) {
-      return MHD_NO;
-    }
-    body = owned_body;
     mode = MHD_RESPMEM_MUST_FREE;
     status = MHD_HTTP_OK;
   } else if (strcmp(url, "/") == 0) {
@@ -118,7 +112,7 @@ static MHD_RESULT promhttp_handler(void *cls, struct MHD_Connection *connection,
   struct MHD_Response *response = MHD_create_response_from_buffer(strlen(body), body, mode);
   if (response == NULL) {
     if (mode == MHD_RESPMEM_MUST_FREE) {
-      free(owned_body);
+      free(body);
     }
     ret = MHD_NO;
   } else {